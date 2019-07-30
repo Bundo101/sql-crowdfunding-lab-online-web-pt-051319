b4c4d7090d5e405d15ff1e6d4ed21f5cba3275a2
James Britton <brittonjp@gmail.com>
2019-07-30 15:12:16 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE projects (
-  id INTEGER PRIMARY KEY, 
-  title TEXT, 
-  category TEXT, 
-  funding_goal INTEGER, 
-  start_date DATE, 
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date DATE,
   end_date DATE
 );
 
